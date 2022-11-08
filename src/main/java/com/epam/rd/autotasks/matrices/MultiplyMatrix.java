cae113d1a44cd0ad50b6f340623defc3734af624
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-08 15:34:07 +0300

    Multiply matrix

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -3,10 +3,21 @@ import java.util.Arrays;
 
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        int [][] result = new int [matrix1.length][matrix2[0].length];
+        for (int row = 0; row < result.length; row++) {
+            for (int col = 0; col < result[row].length; col++) {
+                result[row][col] = multiplyMatricesCell(matrix1, matrix2, row, col);
+            }
+        }
+        return result;
+    }
 
-        // Put your code here
-
-        return null;
+    public static int multiplyMatricesCell(int [][] matrix1, int [][] matrix2, int row, int col) {
+        int cell = 0;
+        for (int i = 0; i < matrix2.length; i++) {
+            cell += matrix1[row][i] * matrix2[i][col];
+        }
+        return cell;
     }
 
     public static void main(String[] args) {
